f49f2ea6ee167d63d2afc6f2378ec57b9226ed50
clancycarlsz <ce24aat@herts.ac.uk>
2025-06-24 10:03:06 +0100

    Simple HTML parsing program

---
+++ parse_html.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+
+# Read sample HTML file
+with open('sample/sample.html', 'r', encoding='utf-8') as file:
+    html_content = file.read()
+
+# Parse HTML and count tags
+soup = BeautifulSoup(html_content, 'html.parser')
+tags = soup.find_all()
+tag_counts = {}
+for tag in tags:
+    tag_name = tag.name
+    tag_counts[tag_name] = tag_counts.get(tag_name, 0) + 1
+
+# Print results
+print("HTML Tag Counts:", tag_counts)
